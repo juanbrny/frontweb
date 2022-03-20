82b328b4350eeca2f3627f8b4a4ac617e0053e18
Juan Herrera <juan@brainy.es>
2022-03-20 20:41:36 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "aws_region" {
+  type        = string
+  default     = "eu-west-1"
+  description = "AWS RegionName"
+}
+
+variable "availability_zone" {
+  type        = string
+  default     = "eu-west-1c"
+  description = "AWS AZ Name"
+}
+
+variable "account_id" {
+  type        = string
+  description = "AWS Account ID"
+  sensitive   = true
+}
+
+variable "access_key" {
+  type        = string
+  description = "AWS Access Key"
+  sensitive   = true
+}
+
+variable "secret_key" {
+  type        = string
+  description = "AWS Secrey Key"
+  sensitive   = true
+}
+
+variable "instance_tag" {
+  type        = string
+  description = "Instance name tag"
+}
